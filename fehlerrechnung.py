e85113ed275cf73a5e82e5e138143665a4d2c638
Jan Lucca Viola <janlucca.viola@tu-dortmund.de>
2024-05-06 14:17:30 +0200

    lecker fehler

--- fehlerrechnung.py
+++ fehlerrechnung.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
+import csv
 from numpy import sqrt
 import scipy.constants as const
 from scipy.optimize import curve_fit                        # Funktionsfit:     popt, pcov = curve_fit(func, xdata, ydata) 
@@ -10,10 +11,32 @@ from uncertainties.unumpy import uarray                     # Array von Fehler: 
 from uncertainties.unumpy import (nominal_values as noms,   # Wert:             noms(fehlerwert) = x
                                   std_devs as stds)  
 
-def Blang(I,l):
-    return 12.566*10**(-7)*1*3400*I/l
 
-uI = ufloat(0.5, 0.1)
-ul = ufloat(0.102, 0.005)
+def T(R):
+    return 0.00134*R**2+2.296*R-243.02+273.15
 
-print(Blang(uI, ul))
+Acu= unp.uarray([158.1,153.4,153.6,151.9,153.0,153.6,153.8,154.0,154.3,154.4,154.5,154.7,154.7,154.8,154.8,154.6,155.0,155.0,155.1,155.1,155.2,155.2,155.3,155.3,155.4,155.3,155.3],[0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1])**10**(-3)                    #Spannung Heizspirale cu
+Vcu= unp.uarray([16.56,16.09,16.11,15.92,16.07,16.16,16.19,16.22,16.27,16.29,16.30,16.31,16.33,16.34,16.34,16.36,16.37,16.37,16.38,16.38,16.39,16.39,16,39,16.39,16.39,16.38,16.38],[0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01])
+
+ARez= unp.uarray([150],[])*10**(-3)          #Spannung Heizspirale Rezipient
+Vrez= unp.uarray([],[])
+
+R1start= ufloat(0,0)
+
+R1  = unp.uarray([27.3,28.6,29.2,33.3,37.8,40.4,42.7,47.5,50.0,52.3,55.1,57.6,59.6,61.0,64.1,67.2,69.9,73.8,77.0,80.0,83.6,87.4,91.5,96.0,100.0,104.0],[0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1])
+t1 = unp.uarray([180,309,360,730,1160,1425,1674,2190,2454,2697,2996,3288,3606,3708,4042,4369,4712,5225,5612,5980,6429,7008,7440,8034,8541,9032],[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1])
+
+T1 = T(R1)
+
+print(T(R1[len(R1)-1]))
+print(T(111))
+
+with open('table1.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+
+    # Schreiben Sie die Kopfzeile in die CSV-Datei, wenn gewünscht
+    writer.writerow(['R1', 'T1', 'ARez', 'Vrez', 'R1', 't1'])
+
+    # Schreiben Sie die Inhalte der Arrays in die CSV-Datei
+    for R1, T1, Acu, Vcu, R1, t1 in zip(R1, T1, Acu, Vcu, R1, t1):
+        writer.writerow([R1, T1, Acu, Vcu, R1, t1])
